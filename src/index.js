4707c42199695fda77f388c68ae27682ff740d8e
Amir Naghdi <amirnp.1996@gmail.com>
2022-01-11 17:33:33 +0330

    add Moralis.start()

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import {MoralisProvider} from "react-moralis";
 import {CssBaseline} from "@mui/material";
+import Moralis from "moralis";
 
 const APP_ID = process.env.REACT_APP_MORALIS_APPLICATION_ID;
 const SERVER_URL = process.env.REACT_APP_MORALIS_SERVER_URL;
+
+Moralis.start({appId: APP_ID, serverUrl: SERVER_URL});
 
 ReactDOM.render(
   <MoralisProvider appId={APP_ID} serverUrl={SERVER_URL}>
